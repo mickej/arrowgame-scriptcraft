7bc006137fad48a2ccb7fbe5fefa84efd2b4a2d7
Robert Storlind <carlrobert@users.noreply.github.com>
2015-03-01 21:00:58 +0100

    Uppdaterad för CanaryMod 1.2 (MineCraft 1.8)
    ScriptCraft 3.1.2

--- pil_event.js
+++ pil_event.js
@@ -1,78 +1,106 @@
+'use strict';
+var blocks = require('blocks');
+var utils = require('utils');
+
 // Detta är vår variabel som sparar poängen
 var scores = {}
 
 // Funktion för att hämta poängen. Anropa genom att skriva "/js dartScores()" i chatten.
 exports.dartScores = function() {
-  echo("Poäng");
   for (var player in scores) {
     echo(player + " -> " + scores[player]);
   }
 }
 
+function isWoolly(block) {
+    return block.typeId === blocks.wool.white; // framgår av blocks.js att detta är huvudtypen
+}
+
 // Funktion för att hitta vilket block som träffades. Långt ifrån bra, men får duga så länge. Vi får försöka hitta en bättre lösning :)
-// Detta behövs för att blocket man får ut av eventet ProjectileHitEvent är blocket som pilen befinner sig innuti
+// Detta behövs för att blocket man får ut av händelsen projectileHit är blocket som pilen befinner sig inuti
 // och det är oftast luft, men vi vill veta färgen på ullen.
 var findHitBlock = function(location) {
-  var block = location.world.getBlockAt(location.x, location.y, location.z);
-  if (block.type == 'WOOL') {
+  var x = Math.floor(location.x);
+  var y = Math.floor(location.y); 
+  var z = Math.floor(location.z);
+  var block = location.world.getBlockAt(x, y, z);
+
+  // Träffpunkten först
+  if (isWoolly(block)) {
     return block;
-  } else {
-    for (var i = 1; i <= 3; i++) {
-      block = location.world.getBlockAt(location.x + i, location.y, location.z);
-      if (block.type == 'WOOL') {
-	return block;
-      }
+  }
 
-      block = location.world.getBlockAt(location.x - i, location.y, location.z);
-      if (block.type == 'WOOL') {
-        return block;
-      }
-      
-      block = location.world.getBlockAt(location.x, location.y, location.z + i);
-      if (block.type == 'WOOL') {
-	return block;
-      }
+  for (var i = 1; i <= 3; i++) {
+    
+    block = location.world.getBlockAt(location.x + i, location.y, location.z);
+    if (isWoolly(block)) {
+      return block;
+    }
 
-      block = location.world.getBlockAt(location.x, location.y, location.z - i);
-      if (block.type == 'WOOL') {
-        return block;
-      }
+    block = location.world.getBlockAt(location.x - i, location.y, location.z);
+    if (isWoolly(block)) {
+      return block;
+    }
+	
+    block = location.world.getBlockAt(location.x, location.y + i, location.z);
+    if (isWoolly(block)) {
+      return block;
+    }
 
-      block = location.world.getBlockAt(location.x, location.y + i, location.z);
-      if (block.type == 'WOOL') {
-        return block;
-      }
-
-      block = location.world.getBlockAt(location.x, location.y - i, location.z);
-      if (block.type == 'WOOL') {
-        return block;
-      }
+    block = location.world.getBlockAt(location.x, location.y - i, location.z);
+    if (isWoolly(block)) {
+      return block;
+    }
+	
+    block = location.world.getBlockAt(location.x, location.y, location.z + i);
+    if (isWoolly(block)) {
+      return block;
+    }
+	
+    block = location.world.getBlockAt(location.x, location.y, location.z - i);
+    if (isWoolly(block)) {
+      return block;
     }
   }
-}
+} // findHitBlock
 
 // Detta är koden som anropas när en pil träffar ett block.
 // Vi ska lista ut vilket block som träffades och ge poäng till spelaren som sköt iväg pilen.
-events.on('entity.ProjectileHitEvent', function (listener, event) {
-  var loc = event.entity.location;
-  var block = event.entity.location.world.getBlockAt(loc.x, loc.y, loc.z);
-
-  // Den färg som blev träffad
-  var color = findHitBlock(loc).state.data.color;
-
+events.projectileHit( function (event) {
+  var projectile = event.projectile,
+    shooter = projectile.owner,
+	loc = projectile.location;
+	
+  var block = utils.blockAt(loc);
+   
+  var blockHit = findHitBlock(loc);
+  if ( blockHit === undefined ) {
+    return;
+  }
+  // Den färg som blev träffad finns i attributet "data" (mer info i API:et för BlockType)
+  var dataValue = blockHit.typeId;
+  if (blockHit.data !== 0) {
+     dataValue += ':' + blockHit.data; // t.ex. 35:14
+  }
+  
   // Lägg till spelaren till poänglistan om den inte redan är där
-  if (scores[event.entity.shooter] == undefined) {
-    scores[event.entity.shooter] = 0;
+  if (scores[shooter.name] === undefined) {
+    scores[shooter.name] = 0;
   }
 
   // Här listar vi ut vilken färg vi träffade och ger poäng där efter
-  if (color == 'YELLOW') {
-    scores[event.entity.shooter] += 1;
-  } else if (color == 'RED') {
-    scores[event.entity.shooter] += 2;
-  } else if (color == 'WHITE') {
-    scores[event.entity.shooter] += 4;
-  } else if (color == 'BLACK') {
-    scores[event.entity.shooter] += 8;
+  if (dataValue === blocks.wool.yellow) {
+    scores[shooter.name] += 1;
+  } else if (dataValue === blocks.wool.red) {
+    scores[shooter.name] += 2;
+  } else if (dataValue === blocks.wool.white) {
+    scores[shooter.name] += 4;
+  } else if (dataValue === blocks.wool.black) {
+    scores[shooter.name] += 8;
   }
+  echo(shooter, "New score: " + scores[shooter.name]);
 });
+
+// CanaryMod BlockType: http://docs.visualillusionsent.net/CanaryLib/1.0-RC-3/net/canarymod/api/world/blocks/BlockType.html
+// Test: CanaryMod 1.2 (Minecraft 1.8), ScriptCraft 3.1.2
+
