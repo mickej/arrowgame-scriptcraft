2b8912443d41d969e46fe5a0e014e52e279a78e9
Robert Storlind <carlrobert@users.noreply.github.com>
2015-03-04 18:15:16 +0100

    English comments

--- arrow_event.js
+++ arrow_event.js
@@ -2,10 +2,10 @@
 var blocks = require('blocks');
 var utils = require('utils');
 
-// Detta är vår variabel som sparar poängen
+// This is our score keeping variable
 var scores = {}
 
-// Funktion för att hämta poängen. Anropa genom att skriva "/js dartScores()" i chatten.
+// Function for displaying the score. Call it by typing "/js dartScores()" in the chat.
 exports.dartScores = function() {
   for (var player in scores) {
     echo(player + " -> " + scores[player]);
@@ -13,25 +13,24 @@ exports.dartScores = function() {
 }
 
 function isWoolly(block) {
-    return block.typeId === blocks.wool.white; // framgår av blocks.js att detta är huvudtypen
+    return block.typeId === blocks.wool.white; // This is the main type, according to blocks.js
 }
 
-// Funktion för att hitta vilket block som träffades. Långt ifrån bra, men får duga så länge. Vi får försöka hitta en bättre lösning :)
-// Detta behövs för att blocket man får ut av händelsen projectileHit är blocket som pilen befinner sig inuti
-// och det är oftast luft, men vi vill veta färgen på ullen.
+// Function to find out which block was hit. Far from good, but good enough for now. We need to find a better solution.
+// This is needed since the block returned by the projectileHit event is the block containing the arrow
+// and that is usually air, but we want the color of the wool
 var findHitBlock = function(location) {
   var x = Math.floor(location.x);
   var y = Math.floor(location.y); 
   var z = Math.floor(location.z);
   var block = location.world.getBlockAt(x, y, z);
 
-  // Träffpunkten först
+  // First, try the block reported as hit
   if (isWoolly(block)) {
     return block;
   }
 
   for (var i = 1; i <= 3; i++) {
-    
     block = location.world.getBlockAt(location.x + i, location.y, location.z);
     if (isWoolly(block)) {
       return block;
@@ -64,8 +63,8 @@ var findHitBlock = function(location) {
   }
 } // findHitBlock
 
-// Detta är koden som anropas när en pil träffar ett block.
-// Vi ska lista ut vilket block som träffades och ge poäng till spelaren som sköt iväg pilen.
+// This is the code called when an arrow hits a block.
+// We want to find out which block was hit and award points to the player firing the arrow.
 events.projectileHit( function (event) {
   var projectile = event.projectile,
     shooter = projectile.owner,
@@ -77,18 +76,18 @@ events.projectileHit( function (event) {
   if ( blockHit === undefined ) {
     return;
   }
-  // Den färg som blev träffad finns i attributet "data" (mer info i API:et för BlockType)
+  // The color that was hit is in the "data" attribute (see the BlockType API for details)
   var dataValue = blockHit.typeId;
   if (blockHit.data !== 0) {
-     dataValue += ':' + blockHit.data; // t.ex. 35:14
+     dataValue += ':' + blockHit.data; // for example, 35:14
   }
   
-  // Lägg till spelaren till poänglistan om den inte redan är där
+  // Add the player to the scores list if it's not already in there
   if (scores[shooter.name] === undefined) {
     scores[shooter.name] = 0;
   }
 
-  // Här listar vi ut vilken färg vi träffade och ger poäng där efter
+  // Here, we find out what color was hit and score accordingly
   if (dataValue === blocks.wool.yellow) {
     scores[shooter.name] += 1;
   } else if (dataValue === blocks.wool.red) {
